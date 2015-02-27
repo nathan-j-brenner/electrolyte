1a9bea6089787db4417b45b56fb10577615bbb16
nate <nathan.j.brenner@gmail.com>
2015-02-26 17:21:22 -0800

    changes javaScript so that it opens on the side

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,21 @@ var $nav = $("#nav_panel");
 var $hamburger = $("#hamburger");
 var $mainContent = $(".main_content");
 //hide nav bar
-$nav.hide();
+// $nav.hide();
 
 //show nav bar
-$hamburger.click(function(){
-	$nav.toggle("slow");
+// $hamburger.click(function(){
+// 	$("#something").toggleClass(#active)
+// 	$nav.toggle("slow");
+	// $nav.addClass( )
+	// $( ".main_content" ).last().addClass( "nav_panel" );
 	// $mainContent.css("width", "70%").css("float", "right");
-});
+// });
 
+$hamburger.click(function(){
+      $("#nav_panel").toggleClass("active");
+      $(".main_content").toggleClass("active");
+});
 
 $("#artOne").click(function() {
 	$("#article_one").find();
@@ -28,3 +35,4 @@ $("#artThree").click(function(){
 	$("#article_three").find();
 	$nav.hide("slow");
 });
+
