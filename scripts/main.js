9d9a917a74819155569c485ccfb0ae514d44047d
nate <nathan.j.brenner@gmail.com>
2015-02-26 18:03:01 -0800

    linked nav elements

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,4 @@
-
-//hide and show nav menu
-var $nav = $("#nav_panel");
-var $hamburger = $("#hamburger");
-var $mainContent = $(".main_content");
-//hide nav bar
-// $nav.hide();
-
-//show nav bar
-// $hamburger.click(function(){
-// 	$("#something").toggleClass(#active)
-// 	$nav.toggle("slow");
-	// $nav.addClass( )
-	// $( ".main_content" ).last().addClass( "nav_panel" );
-	// $mainContent.css("width", "70%").css("float", "right");
-// });
-
-$hamburger.click(function(){
+$("#hamburger").click(function(){
       $("#nav_panel").toggleClass("active");
       $(".main_content").toggleClass("active");
 });
-
-$("#artOne").click(function() {
-	$("#article_one").find();
-	$nav.hide("slow");
-});
-
-$("#artTwo").click(function(){
-	$("#article_two").find();
-	$nav.hide("slow");
-});
-
-
-$("#artThree").click(function(){
-	$("#article_three").find();
-	$nav.hide("slow");
-});
-
